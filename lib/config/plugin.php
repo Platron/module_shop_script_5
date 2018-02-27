28867891550d90c9e77aebd39c1a80dbe152218b
lashnag <lexey2006.90@mail.ru>
2018-02-27 13:40:42 +0300

    Update plugin.php

--- lib/config/plugin.php
+++ lib/config/plugin.php
@@ -5,7 +5,7 @@ return array(
     'icon'        => 'img/platron_mini.png',
     'logo'        => 'img/platron.png',
 	'vendor'      => '983191',
-    'version'     => '1.1.4',
+    'version'     => '1.1.5',
     'locale'      => array('ru_RU', ),
     'type'        => waPayment::TYPE_ONLINE,
 );
