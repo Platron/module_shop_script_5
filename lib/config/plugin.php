a8e5a427a2cf06cc7a953f7f4658aed6c5d8f3ae
lashnag <lexey2006.90@mail.ru>
2019-01-11 10:46:33 +0300

    New version to webassyst

--- lib/config/plugin.php
+++ lib/config/plugin.php
@@ -4,8 +4,8 @@ return array(
     'description' => 'Платежная система <a target="_blank" href="http://www.platron.ru">Платрон</a>',
     'icon'        => 'img/platron_mini.png',
     'logo'        => 'img/platron.png',
-	'vendor'      => '983191',
-    'version'     => '1.1.5',
+    'vendor'      => '983191',
+    'version'     => '1.1.6',
     'locale'      => array('ru_RU', ),
     'type'        => waPayment::TYPE_ONLINE,
 );
